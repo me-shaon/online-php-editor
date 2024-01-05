d87414983213891be979b407292518696a508394
Ahmed shamim <shaon.cse81@gmail.com>
2024-01-05 12:37:26 +0600

    chore: monaco editor setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
+import defaults from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                mono: [
+                    'Fira Code',
+                    ...defaults.fontFamily.mono,
+                ]
+            }
+        },
     },
     plugins: [],
 }
